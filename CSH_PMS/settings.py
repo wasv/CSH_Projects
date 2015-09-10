97c097869e6ccf886753d2392d2182838d1693ad
William Stevens <wastevensv@gmail.com>
2015-09-09 22:39:01 -0400

    Added remote_user middleware.

--- CSH_PMS/settings.py
+++ CSH_PMS/settings.py
@@ -46,16 +46,23 @@ INSTALLED_APPS = (
     'pms',
 )
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.RemoteUserBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
 
 ROOT_URLCONF = 'CSH_PMS.urls'
 
