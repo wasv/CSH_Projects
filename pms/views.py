0f429ce08cca8d2d04650f4744b3ba64a22d4c9d
William Stevens <wastevensv@gmail.com>
2015-09-13 22:15:08 -0400

    Removed login requirement for loginTest

--- pms/views.py
+++ pms/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
 # Create your views here.
 def loginTest(request):
     return HttpResponse(request.user.username)
