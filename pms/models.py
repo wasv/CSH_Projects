40bde5debfa28110c4c2f81fa8bc631965026d0e
William Stevens <wastevensv@gmail.com>
2015-09-20 21:53:10 -0400

    Models and prepping for views.

--- pms/models.py
+++ pms/models.py
@@ -1,3 +1,31 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Project(models.Model):
+    STATE_CHOICES = (
+        ('C','Concept'),
+        ('O','Ongoing'),
+        ('D','Done'),
+        ('A','Abandoned')
+    )
+    title = models.CharField(max_length=140)
+    description = models.TextField()
+    last_update = models.DateField(timezone.now())
+    state = models.CharField(max_length=1, choices=STATE_CHOICES)
+
+    owner = models.ForeignKey(Profiles)
+    contributors = models.ManyToManyField(Profiles)
+
+    website = models.URLField()
+    documentation = models.URLField()
+
+    def is_active(self):
+        return self.last_update >= timezone.now() - datetime.timedelta(days=14)
+
+class Profiles(models.Model):
+    user = models.OneToOneField(get_user_model())
+    bio = models.TextField()
+    website = models.URLField()
