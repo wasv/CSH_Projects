778093f77b150a0e54a89b914f97dde82af0d4d8
William Stevens <wastevensv@gmail.com>
2015-09-26 14:37:00 -0400

    Actually enabled project editing. Forgot to set up URL.

--- pms/urls.py
+++ pms/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^login_test/', views.loginTest, name='loginTest'),
-    url(r'^profile/edit/', views.profileCreate, name='profileCreate'),
+    url(r'^login_test/$', views.loginTest, name='loginTest'),
+    url(r'^profile/edit/$', views.profileCreate, name='profileCreate'),
     url(r'^profile/view/(?P<uname>\w+)/', views.profileView, name='profileView'),
-    url(r'^project/new/', views.projectCreate, name='projectCreate'),
-    url(r'^project/edit/(?P<project_id>[0-9]+)/', views.projectDetail, name='projectEdit'),
+    url(r'^project/new/$', views.projectCreate, name='projectCreate'),
+    url(r'^project/edit/(?P<project_id>[0-9]+)/', views.projectEdit, name='projectEdit'),
     url(r'^project/view/(?P<project_id>[0-9]+)/', views.projectDetail, name='projectDetail'),
     url(r'^$', views.index, name='index'),
 ]
