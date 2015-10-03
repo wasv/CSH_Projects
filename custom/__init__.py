24801c01e78862a408bca69074abcfa60fdc048a
William Stevens <wastevensv@gmail.com>
2015-10-03 17:42:49 -0400

    Made remote user debug hack a bit less hacky.

--- custom/__init__.py
+++ custom/__init__.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
 from CSH_PMS import settings
+import os
 __author__ = 'William'
 
 
 class CustomHeaderMiddleware(RemoteUserMiddleware):
     header = 'HTTP_WEBAUTH_USER'
     def process_request(self, request):
-        if settings.DEBUG:
-            request.META[self.header] = 'wasv'
+        if settings.DEBUG and hasattr(os.environ,'REMOTE_USER'):
+            request.META[self.header] = os.environ['REMOTE_USER']
         return RemoteUserMiddleware.process_request(self, request)
