41000da37f81fb53aec7c35f750dffc5dabc5651
William Stevens <wastevensv@gmail.com>
2015-10-03 17:28:37 -0400

    Small hack to allow auth in dev mode.

--- custom/__init__.py
+++ custom/__init__.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
+from CSH_PMS import settings
 __author__ = 'William'
 
 
 class CustomHeaderMiddleware(RemoteUserMiddleware):
     header = 'HTTP_WEBAUTH_USER'
+    def process_request(self, request):
+        if settings.DEBUG:
+            request.META[self.header] = 'wasv'
+        return RemoteUserMiddleware.process_request(self, request)
